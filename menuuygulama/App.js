22ebf9326da1b845440bd1f124d5196ae97fec4c
Atalay Berke Cin <berkecinofficial@gmail.com>
2024-03-19 23:55:42 +0300

    CSS ile main functionda font ve background değişikliği
    CSS ile main functionda test amaçlı font ve background değişikliği yapıldı.

--- menuuygulama/App.js
+++ menuuygulama/App.js
@@ -1,13 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const courses=[
+    {name:'Çin Mutfağı',id:1},
+    {name:'Makarnalar',id:2},
+    {name:'Tavuk Çeşitleri',id:3},
+    {name:'Aperatifler',id:4},
+    {name:'Et Çeşitleri',id:5}
+  ]
+
   return (
     <View style={styles.container}>
-      <Text>Menü Uygulaması/Atalay Berke Cin</Text>
+      <Text style={styles.title}>Restaurant Menü</Text>
+      <Text style={styles.content}>ATALAY BERKE CİN</Text>
       <StatusBar style="auto" />
     </View>
   );
+  
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +27,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor:'aqua',
+    
+   
   },
+  title:{
+    fontSize:40,
+    color:'white',
+    marginBottom:15,
+    
+  },
+  content:{
+    fontSize:25,
+  }
 });

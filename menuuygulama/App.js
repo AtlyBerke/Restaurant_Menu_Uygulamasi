54eef999abb6c6309a46958aaf131556cb677fa7
Atalay Berke Cin <berkecinofficial@gmail.com>
2024-04-19 20:15:29 +0300

    React Navigation Kurulum
    Sayfalar arası yönlendirme yapabilmemiz için projeme react navigation kurdum.
    'npm' komutuyla gerekli paketleri yükledikten sonra projemin
    'App.js' kısmına bu paketleri import ettim. ve Kategori Ekranını React Navigation kısmına yansıttım.

--- menuuygulama/App.js
+++ menuuygulama/App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
 import CategoriesScreen from './screen/CategoriesScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (<CategoriesScreen/>);
-  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Categories" component={CategoriesScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {

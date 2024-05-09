dcdb039d8fe1667c9601f1a56e3c07e6dea37e7a
Atalay Berke Cin <berkecinofficial@gmail.com>
2024-05-09 23:43:41 +0300

    Yemek Detay Sayfasına CSS ekleme
    Kategoriye göre gelen yemeklerin içinde yemeğe ait bilgileri içeren sayfasına CSS'lerini atadım.

--- menuuygulama/components/FoodIngredients.js
+++ menuuygulama/components/FoodIngredients.js
@@ -3,10 +3,21 @@ import React from 'react';
 
 export default function FoodIngredients({ data }) {
   return data.map((dataIng) => (
-    <View key={dataIng}>
-      <Text>{dataIng}</Text>
+    <View key={dataIng} style={styles.listItem}>
+      <Text style={styles.itemText}>{dataIng}</Text>
     </View>
   ));
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  listItem: {
+    backgroundColor: 'yellow',
+    marginVertical: 5,
+    marginHorizontal: 14,
+    borderRadius: 10,
+    paddingVertical: 7,
+  },
+  itemText: {
+    textAlign: 'center',
+  },
+});
--- menuuygulama/screen/FoodDetailScreen.js
+++ menuuygulama/screen/FoodDetailScreen.js
@@ -10,27 +10,40 @@ export default function FoodDetailScreen({ route }) {
   console.log(selectedFood);
 
   return (
-    <ScrollView>
-    <Image style={styles.image} source={{ uri: selectedFood.imageUrl }} />
-    <View style={styles.details}>
+    <ScrollView style={styles.rootContainer}>
+      <Image style={styles.image} source={{ uri: selectedFood.imageUrl }} />
+      <Text style={styles.title}>{selectedFood.title}</Text>
+      <View style={styles.details}>
         <Text style={styles.detailItem}>{selectedFood.complexity}</Text>
         <Text style={styles.detailItem}>{selectedFood.affordability}</Text>
-    </View>
-    <View>
-        <View>
-          <Text>İçinkiler</Text>
-        </View>
-        <FoodIngredients data={selectedFood.ingredients}/>
       </View>
-  </ScrollView>
+      <View style={styles.listContainer}>
+        <View style={styles.subTitleContainer}>
+          <Text style={styles.subTitle}>İçindekiler</Text>
+        </View>
+        <FoodIngredients data={selectedFood.ingredients} />
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  rootContainer: {
+    marginBottom: 50,
+  },
+
   image: {
     width: '100%',
     height: 300,
   },
+  title: {
+    color:'black',
+    textAlign: 'center',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 5,
+  },
+
   details: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -38,7 +51,23 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
   detailItem: {
-    marginHorizontal: 4,
-    fontSize: 12,
+    marginHorizontal: 5,
+    fontSize: 15,
+    color: 'black',
+  },
+  listContainer: {
+    width: '100%',
+    paddingHorizontal: 10,
+  },
+  subTitleContainer: {
+    alignItems: 'center',
+    borderBottomWidth: 2,
+    borderBottomColor: 'red',
+    marginVertical: 5,
+  },
+  subTitle: {
+    color: 'red',
+    fontSize: 24,
+    fontWeight: 'bold',
   },
 });
